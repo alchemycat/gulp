2fab6c58203cfb92101f1f833d346f1881ee1030
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-15 11:27:32 +0300

    add reset

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,6 +1,5 @@
 import webpack from "webpack-stream";
 
-
 export const js = () => {
     return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
     .pipe(app.plugins.plumber(
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.isWebp();
+document.addEventListener("DOMContentLoaded", () => {
+	flsFunctions.isWebp();
+});
